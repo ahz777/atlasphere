d808321dca37e32417bf39a45e0a5aca1ac8dd99
ahz777 <abdallah.hz777@gmail.com>
2025-10-23 17:07:02 +0300

    Adding a Custom Form Input Component &

---
+++ frontend/src/pages/NewPlace.jsx
@@ -0,0 +1,19 @@
+import Input from "../components/UI/Input";
+
+import "./NewPlace.css";
+
+const NewPlaces = () => {
+  return (
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[]}
+        errorText="Please enter a valid title."
+      />
+    </form>
+  );
+};
+
+export default NewPlaces;
